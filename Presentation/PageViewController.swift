d7db503a3e25a8087ec93f1bd881dd5147815d55
Valeriy <ya_valerko@mail.ru>
2019-06-15 15:49:06 +0600

    03 end of lesson 2

--- Presentation/PageViewController.swift
+++ Presentation/PageViewController.swift
@@ -9,22 +9,38 @@
 import UIKit
 
 class PageViewController: UIPageViewController {
+    
+    let presentScreenContent = [
+        "Первая страница презентации, рассказывает о том, что наша страница представляет",
+        "Второая страница презентации, рассказывает о какой удобной фишке приложени",
+        "Третья страница презентации тоже рассказывает о чем то интересном",
+        "Ну и наконец последняя страница призентации с напуствием в добрый путь"
+    ]
+    
+    let emojiArray = ["👻", "🤩", "🤪", "😇"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        if let contentViewController = showViewControllerAtIndex(0) {
+            setViewControllers([contentViewController], direction: .forward, animated: true, completion: nil)
+        }
+    }
+    
+    func showViewControllerAtIndex(_ index: Int) -> ContentViewController? {
+        guard index >= 0 else { return nil }
+        guard index < presentScreenContent.count else { return nil }
+        guard let contentViewController = storyboard?.instantiateViewController(
+            withIdentifier: "ContentViewController") as? ContentViewController else { return nil }
+        
+        contentViewController.presentText = presentScreenContent[index]
+        contentViewController.emoji = emojiArray[index]
+        contentViewController.currentPage = index
+        contentViewController.numberOfPage = presentScreenContent.count
+        
+        return contentViewController
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
