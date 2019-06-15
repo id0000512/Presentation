8cbbe59a6827701b917640510137df970d4b6413
Valeriy <ya_valerko@mail.ru>
2019-06-15 14:33:48 +0600

    02 end of lesson 1

---
+++ Presentation/ContentViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ContentViewController.swift
+//  Presentation
+//
+//  Created by Valeriy Pokatilo on 15/06/2019.
+//  Copyright © 2019 Valeriy Pokatilo. All rights reserved.
+//
+
+import UIKit
+
+class ContentViewController: UIViewController {
+
+    @IBOutlet weak var presentTextLabel: UILabel!
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
