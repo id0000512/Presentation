13568e11e73904783379870e1b86e12bb012e360
Valeriy <ya_valerko@mail.ru>
2019-06-15 14:14:13 +0600

    01 before starting

--- Presentation/ViewController.swift
+++ Presentation/ViewController.swift
@@ -1,6 +1,7 @@
-//
+//  Based on UIPageViewController
 //  ViewController.swift
 //  Presentation
+//
 //
 //  Created by Valeriy Pokatilo on 15/06/2019.
 //  Copyright © 2019 Valeriy Pokatilo. All rights reserved.
@@ -12,7 +13,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+
+
     }
 
 
